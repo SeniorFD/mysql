SeniorFD/mysql

commit 8189925374e7ce3819bbe88b64c7b15abac96b16

Removed slow unnecessary stack access
https://github.com/felixge/node-mysql/issues/439

lib/protocol/sequences/Sequence.js
@@ -12,7 +12,7 @@ function Sequence(callback) {
   this._ended            = false;
 
   // Experimental: Long stack trace support
-  this._callSite = (new Error).stack.replace(/.+\n/, '');
+  this._callSite = new Error;
 }
 
 Sequence.determinePacket = function(byte) {
@@ -41,7 +41,7 @@ Sequence.prototype._addLongStackTrace = function(err) {
     return;
   }
 
-  err.stack += delimiter + this._callSite;
+  err.stack += delimiter + this._callSite.stack.replace(/.+\n/, '');
 };
 
 Sequence.prototype.end = function(err) {

commit 10731f01aa3a05801fa9d0afb30d6c36d1a5a444

Fix memory leak
This problem was introduced by 8189925374e7ce3819bbe88b64c7b15abac96b16
as determined by git bisect: https://gist.github.com/felixge/5567854

Fixes #471

lib/protocol/sequences/Sequence.js
@@ -55,6 +55,13 @@ Sequence.prototype.end = function(err) {
     this._addLongStackTrace(err);
   }
 
+  // Without this we are leaking memory. This problem was introduced in
+  // 8189925374e7ce3819bbe88b64c7b15abac96b16. I suspect that the error object
+  // causes a cyclic reference that the GC does not detect properly, but I was
+  // unable to produce a standalone version of this leak. This would be a great
+  // challenge for somebody interested in difficult problems : )!
+  delete this._callSite;
+
   // try...finally for exception safety
   try {
     if (err) {